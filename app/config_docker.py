d0ac85205d8fc857cc6bb0b92229b0b78552bd9e
Stanislav Ratashnyuk <hmstr@yandex-team.ru>
2019-02-25 21:00:34 +0300

    prepare for deploy, fix scripts

--- app/config_docker.py
+++ app/config_docker.py
@@ -6,5 +6,5 @@ class Config(object):
 	POSTGRES_USER = os.environ.get("POSTGRES_USER")
 	POSTGRES_PW = os.environ.get("POSTGRES_PW")
 	POSTGRES_URL = "rc1b-879g3ztam61ufn9g.mdb.yandexcloud.net"
-	POSTGRES_DB = "assessment"
-	SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}:{port}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB, port=12001)
+	POSTGRES_DB = "db1"
+	SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}:{port}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB, port=6432)
--- start_app.sh
+++ start_app.sh
@@ -5,5 +5,5 @@ source .venv/bin/activate
 export FLASK_APP=assessment.py
 export FLASK_DEBUG=0
 
-uwsgi --http 127.0.0.1:5000 --module assessment:app
+uwsgi --http 0.0.0.0:5000 --module assessment:app
 
